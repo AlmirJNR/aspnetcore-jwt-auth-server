3fc3fb69835f6f34e2f6f81514aec1cf3c5050d5
Almir Alves de Freitas Junior <almirafjr.dev@gmail.com>
2022-09-09 18:45:29 -0300

    feat: add created_at column to app_user table to Initial.sql

--- Assets/Initial.sql
+++ Assets/Initial.sql
@@ -2,5 +2,6 @@ CREATE TABLE IF NOT EXISTS app_user
 (
     id       UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     email    TEXT UNIQUE NOT NULL,
-    password TEXT NOT NULL
+    password TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW()
 );
